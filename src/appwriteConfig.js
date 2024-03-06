d4aadc08556cc61f49b9c2271926025b7a353f88
Bartek Bartosik <b.bartek987@gmail.com>
2024-03-06 18:03:50 +0100

    checking env

--- src/appwriteConfig.js
+++ src/appwriteConfig.js
@@ -1,9 +1,12 @@
 import { Client, Databases, Account } from "appwrite";
 
-export const PROJECT_ID = import.meta.env.VITE_PROJECT_ID;
-export const DB_ID = import.meta.env.VITE_DB_ID;
-export const COLLECTION_ID_MESSAGES = import.meta.env
-  .VITE_COLLECTION_ID_MESSAGES;
+// export const PROJECT_ID = import.meta.env.VITE_PROJECT_ID;
+// export const DB_ID = import.meta.env.VITE_DB_ID;
+// export const COLLECTION_ID_MESSAGES = import.meta.env
+//   .VITE_COLLECTION_ID_MESSAGES;
+export const PROJECT_ID = " 65e4ab8f4b678657ff73";
+export const DB_ID = "65e6595c8482e575f11e";
+export const COLLECTION_ID_MESSAGES = "65e6596caea47768ff4a";
 
 const client = new Client();
 client
